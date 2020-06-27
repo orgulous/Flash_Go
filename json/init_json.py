6ad72cc1fa4b8995e8017c318855ce2b3f34a081
root <root@Luke.localdomain>
2020-06-26 23:39:01 -0400

    added the initial json

---
+++ json/init_json.py
@@ -0,0 +1,16 @@
+import datetime
+import json
+
+time = datetime.datetime.now()
+time = time.strftime("%c")	
+	
+x = {
+	"filename": "null.sgf",
+	"date_created": time,
+	"space_level" : 0,
+	"date_last_reviewed": time,
+	"date_due": time
+}
+
+with open('card_data.json', 'w') as outfile:
+    json.dump(x, outfile)
